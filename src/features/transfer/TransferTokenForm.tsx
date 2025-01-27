f847a4367df2340fb343fcb71e312c09e3a4d69d
fernandomg <fernando.greco@gmail.com>
2025-01-27 23:11:44 +0100

    feat(intents): remove unused methods from CoreProxy
    Also, allow to pass `fillDeadline` for intent creation

--- src/features/transfer/TransferTokenForm.tsx
+++ src/features/transfer/TransferTokenForm.tsx
@@ -575,6 +575,7 @@ function useFormInitialValues(): TransferFormValues {
 
 const insufficientFundsErrMsg = /insufficient.[funds|lamports]/i;
 const emptyAccountErrMsg = /AccountNotFound/i;
+const DEFAULT_FILL_DEADLINE = Math.floor(Date.now() / 1000) + 60 * 3;
 
 async function validateForm(
   warpCore: WarpCore,
@@ -597,6 +598,7 @@ async function validateForm(
       recipient,
       sender: address || '',
       senderPubKey: await senderPubKey,
+      fillDeadline: values.fillDeadline ?? DEFAULT_FILL_DEADLINE,
     });
     return result;
   } catch (error: any) {
